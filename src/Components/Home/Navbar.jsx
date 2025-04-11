118c3775af726ebc80948b775e9c18d2b96db1da
Uddyan Sahu <156681279+uddyan85@users.noreply.github.com>
2025-04-11 20:24:20 +0530

    Update Navbar.jsx

--- src/Components/Home/Navbar.jsx
+++ src/Components/Home/Navbar.jsx
@@ -72,7 +72,7 @@ const Navbar = () => {
             initial={false}
             animate={nav ? "open" : "closed"}
             variants={menuVariant}
-            className="fixed left-0 top-0 w-full min-h-screen bg-opacity-70 backdrop-blur-md z-40">
+            className="fixed left-0 top-0 w-full min-h-screen bg-purpule-950/40 bg-opacity-70 backdrop-blur-md z-40">
 
             <ul className="font-semibold text-4xl space-y-10 mt-24 text-center cursor-pointer">
            <li>
